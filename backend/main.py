aede5ee934b2d57ca463a06f8c77c7de414b11ca
NeoScript <neoscripting@gmail.com>
2022-05-21 04:14:01 +0000

    cinitialize an application using FastAPI

---
+++ backend/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+
+def create_app() -> FastAPI:
+    application = FastAPI()
+    return application
+
+
+app = create_app()
+
+
+@app.get('/')
+def index():
+    return {"projectName": "star-gaze"}
